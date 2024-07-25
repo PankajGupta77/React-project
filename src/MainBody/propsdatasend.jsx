8b042fe74e7587d0129f25a8df4ef8fce73c367d
Pankaj Gupta <Pankaj Gupta@inindcrws.com>
2024-07-25 14:52:17 +0530

    done

--- src/MainBody/propsdatasend.jsx
+++ src/MainBody/propsdatasend.jsx
@@ -135,7 +135,10 @@ const PropsDataSend = () => {
               </div>
               <div className="modal-body">
               <iframe   width="100%"
-                  height="400px" src="https://www.youtube.com/embed/HVjjoMvutj4?si=0uA20xPKqL73S-Z9" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+                  height="400px" src="https://www.youtube.com/embed/HVjjoMvutj4?si=0uA20xPKqL73S-Z9"
+                   title="YouTube video player" frameborder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+                    referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
               </div>
               <div className="modal-footer">
                 <button type="button" className="btn btn-secondary" onClick={handleClose}>
