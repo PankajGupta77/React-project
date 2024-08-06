302c4d9d7d1d7d6b266f732a1a9c24665ecfcbf7
Pankaj Gupta <Pankaj Gupta@inindcrws.com>
2024-08-06 13:24:07 +0530

    Add Netlify configuration to use legacy peer deps

--- src/header/TermsAndConditions.jsx
+++ src/header/TermsAndConditions.jsx
@@ -1,54 +1,54 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
+import './footer.css'
 const TermsAndConditions = () => {
   return (
     <div className="container py-5">
       <h4>Terms and Conditions</h4>
       
-      <section>
+      <section className='condition'>
         <h5>Introduction:</h5>
         <p>
           Welcome to LearnXpert! These terms and conditions outline the rules and regulations for the use of LearnXpert's website. By accessing this website, we assume you accept these terms and conditions. Do not continue to use LearnXpert if you do not agree to take all of the terms and conditions stated on this page.
         </p>
       </section>
       
-      <section>
+      <section className='condition'>
         <h5>Platform Overview:</h5>
         <p>
           These Terms and Conditions (“Terms”) set out the terms and conditions for the use of the Site, the mobile application(s) (the “Application”), and any features, subdomains, content, functionality, products, services, media, applications, or solutions offered on or through the Site and/or the Application (collectively referred to as the “Platform” or “LearnXpert Platform”).
         </p>
       </section>
 
-      <section>
+      <section className='condition'>
         <h5>Scope of Terms:</h5>
         <p>
           These Terms apply to all users of the Platform, including Content Providers who create and/or make available Content on the Platform and Learners who access the Platform to learn a subject or skill. These Terms, along with the Privacy Policy, Refund Policy, Cookies Policy, Accessibility Statement, and any other relevant terms and conditions, define the relationship between you and LearnXpert and govern your use of the Platform and Services.
         </p>
       </section>
 
-      <section>
+      <section className='condition'>
         <h5>Acceptance of Terms:</h5>
         <p>
           By visiting and accessing the Platform, providing your Personal Information, using the Services, or otherwise signaling your agreement when the option is presented to you, you accept and agree to these Terms. If you do not agree to any of the terms or do not wish to be bound by them, then please do not use the Platform.
         </p>
       </section>
 
-      <section>
+      <section className='condition'>
         <h5>About LearnXpert:</h5>
         <p>
           When we refer to “LearnXpert”, “we”, “us”, and “our”, we mean Sorting Hat Technologies Private Limited – a company incorporated in India and its affiliates licensed to host the Platform and offer Services. Refer to the 'About LearnXpert and the Platform' section to know which LearnXpert entity your Agreement will be with.
         </p>
       </section>
 
-      <section>
+      <section className='condition'>
         <h5>Definitions:</h5>
         <p>
           All capitalized terms are defined or have the meaning assigned under these Terms. If you find any capitalized word, it means that its definition is provided in the context of these Terms or in the relevant Platform Terms.
         </p>
       </section>
 
-      <section>
+      <section className='condition'>
         <h5>Refunds and Cancellations:</h5>
         <p>
           Refunds and cancellations are aligned with the products and services available on the LearnXpert Platform. The processing time for refunds is typically 5-7 working days. If you encounter any issues or require a refund, please contact us at pg007135@gmail.com with your transaction ID or order ID. We will assist you as promptly as possible.
